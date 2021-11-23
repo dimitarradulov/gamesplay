a78b93af305657ecb71d7b95e13ea6482db57555
Dimitar Radulov <dimitaradulov@gmail.com>
2021-11-23 14:04:31 +0200

    Added header component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
+import Header from './components/Header';
+
 function App() {
   return (
     <div id="box">
-      <header>
-        <h1>
-          <a className="home" href="/#">
-            GamesPlay
-          </a>
-        </h1>
-        <nav>
-          <a href="/#">All games</a>
-          <div id="user">
-            <a href="/#">Create Game</a>
-            <a href="/#">Logout</a>
-          </div>
-          <div id="guest">
-            <a href="/#">Login</a>
-            <a href="/#">Register</a>
-          </div>
-        </nav>
-      </header>
+      <Header />
 
       <main id="main-content"></main>
 
